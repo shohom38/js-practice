6da2ac7e9069b5c7eecbdc6ade1ce39dd6aaf5ac
shohom38 <shohom38@naver.com>
2021-02-11 00:49:27 +0900

    add js code #1

---
+++ main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById('demo2').innerHTML = "Hi my name is sum"
+}
+
+document.getElementById('plus').innerHTML = 5 + 6;
+document.write("wowowowow");
+document.write(7 + 7);
+alert("Boo!");
---
+++ syntax.js
@@ -0,0 +1,16 @@
+document.getElementById("demo").innerHTML = 
+"이것은 문자입니다";
+
+var x, y;
+x = 10;
+y = 1506;
+
+document.getElementById("demo1").innerHTML = 
+x * y * 10000;
+
+var price1 = 15000;
+var price2 = 32000;
+var totalprice = price1 + price2;
+
+document.getElementById("demo2").innerHTML =
+"가격의 총합은" + " " + totalprice + "원" + " " + "입니다.";
